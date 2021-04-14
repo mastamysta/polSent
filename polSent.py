a8e766c395e8bf96a21489c6fd988055c3cc8b35
mastamysta <benjamin-read@hotmail.co.uk>
2021-04-14 10:29:59 +0100

    Added tsv reader

--- polSent.py
+++ polSent.py
@@ -1,7 +1,12 @@
 import pandas as pd
 import numpy as np
 
+#returns dataframe cotaining lexicon
+def getLexicon():
+    return pd.read_csv("politics.tsv",sep='\t')
+
 def test():
+    lex = getLexicon()
     print("Test Executed")
 
 if __name__ == '__main__':
